6b355fc904680e596f25f738fb3d9fcbffd6c6f8
pereirathiago <pereira.tp.thiago@gmail.com>
2023-05-04 15:40:20 -0300

    update

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,25 @@
+import { Category } from "../model/Category"
+
+class CategoriesRepository{
+    private categories: Category[]
+
+    constructor() {
+        this.categories = []
+    }
+
+    create() {
+        const category = new Category() 
+
+        Object.assign(category, {
+            name,
+            description,
+            created_at: new Date()
+        })
+
+        this.categories.push(category)
+
+        return res.status(201).json({category})
+    }
+}
+
+export { CategoriesRepository }
