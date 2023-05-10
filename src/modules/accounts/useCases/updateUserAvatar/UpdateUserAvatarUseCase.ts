1142be9cb2b743f9c10a8a3bb58f92470db05b45
pereirathiago <pereira.tp.thiago@gmail.com>
2023-05-10 09:12:36 -0300

    02-05 avatar de usuario

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -5,27 +5,32 @@
 // criar regra de negocio do upload
 // criar controller
 
-import { inject } from "tsyringe"
+import { inject, injectable } from "tsyringe"
 import { IUsersRepository } from "../../repositories/IUsersRepository"
+import { deleteFile } from "../../../../utils/file"
 
 interface IRequest {
   user_id: string
   avatar_file: string
 }
 
-
+@injectable()
 class UpdateUserAvatarUseCase {
 
   constructor(
-    @inject("UserRepository")
+    @inject("UsersRepository")
     private usersRepository: IUsersRepository
-  ) {}
+  ) { }
 
-  async execute({ user_id, avatar_file }: IRequest) {
+  async execute({ user_id, avatar_file }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id)
 
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`)
+    }
+
     user.avatar = avatar_file
-  
+
     await this.usersRepository.create(user)
   }
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,18 +1,19 @@
-import { Router } from "express";
-import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
-import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
-import multer from "multer";
+import { Router } from "express"
+import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController"
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController"
+import multer from "multer"
+
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated"
+import uploadConfig from "../config/upload"
 
 const usersRoutes = Router()
 
-const upload = multer({
-  dest: "./avatar"
-})
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"))
 
 const createUserController = new CreateUserController()
 const updateUserAvatarController = new UpdateUserAvatarController()
 
 usersRoutes.post("/", createUserController.handle)
-usersRoutes.patch("/avatar", upload.single("file"), updateUserAvatarController.handle)
+usersRoutes.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"), updateUserAvatarController.handle)
 
 export { usersRoutes }
