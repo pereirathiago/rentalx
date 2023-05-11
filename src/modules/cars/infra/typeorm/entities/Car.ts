abfbef45a95998cf19e4af20524fcf5a3e802d48
pereirathiago <pereira.tp.thiago@gmail.com>
2023-05-11 15:00:22 -0300

    03-03-11 nao finalizado

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,6 +1,7 @@
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm"
+import { Column, CreateDateColumn, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, PrimaryColumn } from "typeorm"
 import { v4 as uuidV4 } from "uuid"
 import { Category } from "./Category"
+import { Specification } from "./Specification"
 
 @Entity("cars")
 class Car {
@@ -25,6 +26,13 @@ class Car {
   category: Category
   @Column()
   category_id: string
+  @ManyToMany(() => Specification)
+  @JoinTable({
+    name: "specifications_cars",
+    joinColumns: [{name: "car_id"}],
+    inverseJoinColumns: [{name: "specification_id"}]
+  })
+  specifications: Specification[]
   @CreateDateColumn()
   created_at: Date
 
