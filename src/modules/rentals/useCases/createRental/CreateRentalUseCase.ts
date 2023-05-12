a8053bccf2df7f099f345fa4188179f967c56c70
pereirathiago <pereira.tp.thiago@gmail.com>
2023-05-12 14:57:45 -0300

    03-04-02 nao finalizado

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -0,0 +1,16 @@
+
+interface IRequest {
+  user_id: string
+  car_id: string
+  expected_return_data: Date
+}
+
+class CreateRentalUseCase {
+  constructor(private rentalsRepository: IRentalsRepository) {}
+
+  async execute({ user_id, car_id, expected_return_data }: IRequest): Promise<void> {
+    const carAvailable = await this.rentalsRepository.findByCar(car_id)
+  }
+}
+
+export { CreateRentalUseCase }
