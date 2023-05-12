6cdeaa88a8a0bf4dd4cd6bdec27d2754375c3f77
pereirathiago <pereira.tp.thiago@gmail.com>
2023-05-12 14:19:09 -0300

    03-04-01

--- src/shared/infra/typeorm/migrations/1683636721659-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1683636721659-CreateUsers.ts
@@ -9,7 +9,8 @@ export class CreateUsers1683636721659 implements MigrationInterface {
 				columns: [
 					{
 						name: "id",
-						type: "uuid"
+						type: "uuid",
+						isPrimary: true
 					},
 					{
 						name: "name",
