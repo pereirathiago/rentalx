e589fb0cfb22ded5760e02aae67ef23d6be409a6
pereirathiago <pereira.tp.thiago@gmail.com>
2023-05-15 13:50:37 -0300

    03-04-06

---
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -0,0 +1,31 @@
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { Rental } from "../entities/Rental";
+import { Repository, getRepository } from "typeorm";
+
+class RentalsRepository implements IRentalsRepository {
+  private repository: Repository<Rental>
+
+  constructor() {
+    this.repository = getRepository(Rental)
+  }
+
+  async create({ car_id, user_id, expected_return_date }: ICreateRentalDTO): Promise<Rental> {
+    
+    const rental = this.repository.create({ car_id, user_id, expected_return_date })
+    console.log(rental)
+    await this.repository.save(rental)
+    return rental
+  }
+  async findOpenRentalByCar(car_id: string): Promise<Rental> {
+    const openByCar = await this.repository.findOne({ car_id })
+    return openByCar
+  }
+  async findOpenRentalByUser(user_id: string): Promise<Rental> {
+    const openByUser = await this.repository.findOne({ user_id })
+    return openByUser
+  }
+
+}
+
+export { RentalsRepository }
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,5 @@
 import { container } from "tsyringe"
+import "@shared/container/providers"
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository"
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository"
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationRepository"
@@ -9,22 +10,13 @@ import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository"
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository"
 import { ICarImageRepository } from "@modules/cars/repositories/ICarImageRepository"
 import { CarsImageRepository } from "@modules/cars/infra/typeorm/repositories/CarsImageRepository"
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository"
+import { RentalsRepository } from "@modules/rentals/infra/typeorm/repositories/RentalsRepository"
 
 // ICategoriesRepository
-container.registerSingleton<ICategoriesRepository>(
-	"CategoriesRepository",
-	CategoriesRepository
-)
-
-container.registerSingleton<ISpecificationsRepository>(
-	"SpecificationRepository",
-	SpecificationRepository
-)
-
-container.registerSingleton<IUsersRepository>(
-	"UsersRepository",
-	UsersRepository
-)
-
+container.registerSingleton<ICategoriesRepository>("CategoriesRepository", CategoriesRepository)
+container.registerSingleton<ISpecificationsRepository>("SpecificationRepository", SpecificationRepository)
+container.registerSingleton<IUsersRepository>("UsersRepository", UsersRepository)
 container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository)
 container.registerSingleton<ICarImageRepository>("CarsImageRepository", CarsImageRepository)
+container.registerSingleton<IRentalsRepository>("RentalsRepository", RentalsRepository)
