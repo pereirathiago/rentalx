2e444f15c29501549ee6695ae7a38bcac60e443d
pereirathiago <pereira.tp.thiago@gmail.com>
2023-05-16 10:40:55 -0300

    04-01-03

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -5,6 +5,7 @@ interface IRentalsRepository {
   create({ car_id, user_id, expected_return_date }: ICreateRentalDTO): Promise<Rental>
   findOpenRentalByCar(car_id: string): Promise<Rental>
   findOpenRentalByUser(user_id: string): Promise<Rental>
+  findById(id: string): Promise<Rental>
 }
 
 export { IRentalsRepository }
