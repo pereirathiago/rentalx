548149d2d8436306f64707f8daf9382578d9e8fc
pereirathiago <pereira.tp.thiago@gmail.com>
2023-05-16 11:16:06 -0300

    04-01-04

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -10,19 +10,18 @@ class RentalsRepository implements IRentalsRepository {
     this.repository = getRepository(Rental)
   }
 
-  async create({ car_id, user_id, expected_return_date }: ICreateRentalDTO): Promise<Rental> {
+  async create({ car_id, user_id, expected_return_date, id, end_date, total }: ICreateRentalDTO): Promise<Rental> {
     
-    const rental = this.repository.create({ car_id, user_id, expected_return_date })
-    console.log(rental)
+    const rental = this.repository.create({ car_id, user_id, expected_return_date, id, end_date, total })
     await this.repository.save(rental)
     return rental
   }
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    const openByCar = await this.repository.findOne({ car_id })
+    const openByCar = await this.repository.findOne({ where: { car_id, end_date: null } })
     return openByCar
   }
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    const openByUser = await this.repository.findOne({ user_id })
+    const openByUser = await this.repository.findOne({ where: { user_id, end_date: null } })
     return openByUser
   }
   async findById(id: string): Promise<Rental> {
--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController'
+import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController'
 
 const rentalRoutes = Router()
 
 const createRentalUseCase = new CreateRentalController()
+const devolutionRentalController = new DevolutionRentalController()
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalUseCase.handle)
+rentalRoutes.post("/devolution/:id", ensureAuthenticated, devolutionRentalController.handle)
 
 export { rentalRoutes }
