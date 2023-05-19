a9a5759ad3da43c6b0bd5d92dd303a8bd33ee370
pereirathiago <pereira.tp.thiago@gmail.com>
2023-05-19 09:41:04 -0300

    05-02-04

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -5,7 +5,14 @@ import { IMailProvider } from "./MailProvider/IMailProvider"
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider"
 import { IStorageProvider } from "./StorageProvider/IStorageProvider"
 import { LocalStorageProvider } from "./StorageProvider/implementations/localStorageProvider"
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider"
 
 container.registerSingleton<IDateProvider>("DayjsDateProvider", DayjsDateProvider)
 container.registerInstance<IMailProvider>("EtherealMailProvider", new EtherealMailProvider())
-container.registerSingleton<IStorageProvider>("StorageProvider", LocalStorageProvider)
+
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider
+}
+
+container.registerSingleton<IStorageProvider>("StorageProvider", diskStorage[process.env.disk])
